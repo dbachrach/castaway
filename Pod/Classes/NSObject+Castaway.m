108d5596bae2edd885e6cb00d27bac148d757184
Dustin Bachrach <ahdustin@gmail.com>
2014-07-21 15:04:47 -0700

    Support dynamic return types for the blocks in -match.

--- Pod/Classes/NSObject+Castaway.m
+++ Pod/Classes/NSObject+Castaway.m
@@ -39,6 +39,51 @@ static BOOL CASObjectCastsToBlock(id obj, id block)
     return NO;
 }
 
+/**
+ * Calls a block passing data as its only argument.
+ * Supports calling blocks with any return type, and normalizes it to returning id.
+ * Modified from PromiseKit
+ */
+static id CASCallDynamicBlock(id block, id data)
+{
+    NSMethodSignature *sig = NSMethodSignatureForBlock(block);
+    const char rtype = sig.methodReturnType[0];
+    
+    switch (rtype) {
+        case 'v':
+            ((void(^)(id))block)(data);
+            return nil;
+        case '@':
+            return ((id(^)(id))block)(data);
+        case '*': {
+            char *str = ((char*(^)(id))block)(data);
+            return str ? @(str) : nil;
+        }
+        case 'c': return @(((char(^)(id))block)(data));
+        case 'i': return @(((int(^)(id))block)(data));
+        case 's': return @(((short(^)(id))block)(data));
+        case 'l': return @(((long(^)(id))block)(data));
+        case 'q': return @(((long long(^)(id))block)(data));
+        case 'C': return @(((unsigned char(^)(id))block)(data));
+        case 'I': return @(((unsigned int(^)(id))block)(data));
+        case 'S': return @(((unsigned short(^)(id))block)(data));
+        case 'L': return @(((unsigned long(^)(id))block)(data));
+        case 'Q': return @(((unsigned long long(^)(id))block)(data));
+        case 'f': return @(((float(^)(id))block)(data));
+        case 'd': return @(((double(^)(id))block)(data));
+        case 'B': return @(((_Bool(^)(id))block)(data));
+        case '^':
+            if (strcmp(sig.methodReturnType, "^v") == 0) {
+                ((void(^)(id))block)(data);
+                return nil;
+            }
+            // else fall through!
+        default:
+            @throw [NSException exceptionWithName:@"castaway" reason:@"castaway: unknown return type" userInfo:nil];
+    }
+}
+
+
 @implementation NSObject (Castaway)
 
 - (BOOL(^)(void(^)(id)))as
@@ -66,9 +111,9 @@ static BOOL CASObjectCastsToBlock(id obj, id block)
 
 - (id)match:(NSArray*)patterns
 {
-    for (id(^block)(id) in patterns) {
+    for (id block in patterns) {
         if (CASObjectCastsToBlock(self, block)) {
-            return block(self);
+            return CASCallDynamicBlock(block, self);
         }
     }
     return nil;
